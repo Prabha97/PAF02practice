6f43a9f4329edb5f5b5db489a14b7a5ab39c32a1
Prabha97 <52439268+Prabha97@users.noreply.github.com>
2020-03-07 19:06:36 +0530

    changed main02

--- HelloWorld/src/Main2.java
+++ HelloWorld/src/Main2.java
@@ -4,6 +4,7 @@ public class Main2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("First part has done");
+			System.out.println("second part has done");
 	}
 
 }

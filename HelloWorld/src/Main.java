b1fb83a964b56a8b9ca14739bd4a5d8f76ff343d
Prabha97 <prabhashwari.lk@gmail.com>
2020-03-07 19:03:51 +0530

    changed

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("This is my last project");
+			System.out.println("This is not the last one.");
 	}
 }
